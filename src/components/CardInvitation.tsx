e39a6c83c9ee9b304b2cd72c4e4e51966e1b2fe6
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-07-30 12:47:25 -0500

    cambios pantalla

--- src/components/CardInvitation.tsx
+++ src/components/CardInvitation.tsx
@@ -8,8 +8,8 @@ const CardInvitation = ({ setOpenInvitation }: CardInvitationProps) => {
   };
 
   return (
-    <div className="bg-[url(/fondoMiel.png)] bg-cover bg-no-repeat min-h-screen flex flex-col items-center justify-center gap-10">
-      <img src="DisenoPortadaSinBoton.png" alt="Portada Baby Shower" />
+    <div className="bg-[url(/images/fondoMiel.png)] bg-cover bg-no-repeat min-h-screen flex flex-col items-center justify-center gap-10">
+      <img src="/images/DisenoPortadaSinBoton.png" alt="Portada Baby Shower" />
 
       <button
         className="bg-[#4F99E7] text-white py-2 font-semibold px-4 rounded-2xl border-2 border-white uppercase"
