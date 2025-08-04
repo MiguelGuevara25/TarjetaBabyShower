45dc61e0dd83539a329a5eddf8080b77acba7ed0
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-08-04 18:07:08 -0500

    boton

--- src/components/CardInvitation.tsx
+++ src/components/CardInvitation.tsx
@@ -10,7 +10,7 @@ const CardInvitation = ({ setOpenInvitation }: CardInvitationProps) => {
   return (
     <div className="bg-[url(/images/fondo2.png)] h-dvh bg-no-repeat bg-contain bg-center relative">
       <button
-        className="bg-trasnparent text-[#cba056] py-1 font-semibold px-3 rounded-2xl border-2 border-[#cba056] uppercase absolute top-[33.5%] left-1/2 -translate-x-1/2"
+        className="bg-trasnparent text-[#cba056] py-1 font-semibold px-3 rounded-2xl border-2 border-[#cba056] uppercase absolute top-[31%] left-1/2 -translate-x-1/2"
         onClick={handleInvitation}
       >
         Click aquí
