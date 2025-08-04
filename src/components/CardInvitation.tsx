dec389eac8e169ebaffd4853d9f6115d100b9e8f
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-08-04 17:33:27 -0500

    cambios de boton

--- src/components/CardInvitation.tsx
+++ src/components/CardInvitation.tsx
@@ -10,7 +10,7 @@ const CardInvitation = ({ setOpenInvitation }: CardInvitationProps) => {
   return (
     <div className="bg-[url(/images/fondo2.png)] h-dvh bg-no-repeat bg-contain bg-center relative">
       <button
-        className="bg-[#cba056] text-white py-2 font-semibold px-4 rounded-2xl border-2 border-white uppercase absolute bottom-[17%] left-1/2 -translate-x-1/2"
+        className="bg-[#FFBA3B] text-white py-2 font-semibold px-4 rounded-2xl border-2 border-white uppercase absolute bottom-[17%] left-1/2 -translate-x-1/2"
         onClick={handleInvitation}
       >
         Click aquí
