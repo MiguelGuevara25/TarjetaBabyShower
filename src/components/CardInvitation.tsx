0d1b1f30e2d3efc831c0112c26516b2cb431ef9c
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-08-15 00:30:52 -0500

    animaciones

--- src/components/CardInvitation.tsx
+++ src/components/CardInvitation.tsx
@@ -8,7 +8,7 @@ const CardInvitation = ({ setOpenInvitation }: CardInvitationProps) => {
   };
 
   return (
-    <div className="bg-[url(/images/fondo2.png)] h-dvh bg-no-repeat bg-contain bg-center relative">
+    <div className="bg-[url(/images/fondo2.png)] h-dvh bg-no-repeat bg-contain bg-center relative bg-animated">
       <button
         className="bg-trasnparent text-[#cba056] py-1 font-semibold px-3 rounded-2xl border-2 border-[#cba056] uppercase absolute top-[31%] left-1/2 -translate-x-1/2"
         onClick={handleInvitation}
